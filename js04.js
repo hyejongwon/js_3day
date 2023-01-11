8460628686d83640306587e23a75f44fa30bfe05
wonhj <wonhjteacher@gmail.com>
2023-01-11 13:16:05 +0900

    array ex

---
+++ js04.js
@@ -0,0 +1,22 @@
+var persons = [
+    {
+      name: "a",
+      point: 78,
+      city: "서울",
+    },
+    {
+      name: "b",
+      point: 92,
+      city: "서울",
+    },
+    {
+      name: "c",
+      point: 76,
+      city: "제주",
+    },
+    {
+      name: "d",
+      point: 81,
+      city: "서울",
+    },
+  ];
